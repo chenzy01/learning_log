b9c2bddb0aa7aab99d23d0fb6fb91737c5d891a5
chenzy01 <chenzy.hyx@gmail.com>
2020-08-20 23:34:27 +0800

    add topic url and view

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -20,4 +20,14 @@ $ 美元符号让python查看字符串的结尾
 urlpatterns = [
     url(r'^$', views.index, name='index'),  # 让python查找开头和末尾之间没有任何东西的url
     url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')  # topic_id与某个主题对应，以此来获取
 ]
+
+"""
+r'^topics/(?p<topic_id>\d+)/$' 解析：
+r 表示原始字符串，并指出正则表示式包含在引号内
+/(?p<topic_id>\d+)/  结合前面的 topics 一起匹配，这里的部分是匹配一个整数值，并将值存储在 topic_id的实参中
+() 捕获URL中的值
+?p<topic_id>\d+  将匹配到的值存储到 topic_id 中
+\d+ 匹配任何数字，不管这个数字有多少位
+"""
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -20,3 +20,11 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """显示单个主题极其所有的条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')  # - date_added 前面的减号指定按降序排序
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
